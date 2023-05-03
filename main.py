d8d2037371b200b929f70c1ee58b91915fb55f0a
Sirawit Suwankhatsri <sirawit.suwankhatsri@outlook.com>
2023-05-03 18:11:27 +0700

    modified:   main.py 	new file:   static/resource/Logo.png 	new file:   static/style/home.css 	new file:   static/style/main.css 	new file:   static/style/navbar.css 	modified:   templates/Login.html 	modified:   templates/Register.html

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import ssl
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
 import pymongo
 from my_class.account import Account, Member, Admin
 
@@ -8,10 +8,42 @@ database = my_client.tg_database
 
 app = Flask(__name__)
 
-@app.route('/add_account')
+@app.route('/')
+def html_index():
+   return render_template('Home.html')
+
+@app.route('/login')
+def html_login():
+   return render_template('Login.html')
+
+@app.route('/check_login', methods = ['POST', 'GET'])
+def check_login():
+    if request.method == 'POST':
+      username = request.form['username']
+      password = request.form['password']
+    else:
+       return "Type Error"
+    user_collection = database.user_account
+    output_data = user_collection.find({"username" : username, "password" : password})
+    if any(output_data):
+       return "OK"
+    print("error")
+    return 
+@app.route('/register')
+def html_register():
+   return render_template('Register.html')
+
+@app.route('/add_account', methods = ['POST', 'GET'])
 def add_account():
-    user = Member('name','mypassword',1,'myname','name@email','0124546789')
-#    user = Member('username','password',1,'name','email','phone')
+    if request.method == 'POST':
+      username = request.form['username']
+      password = request.form['password']
+      name = request.form['name']
+      email = request.form['email']
+      phone =  request.form['phone']
+    else:
+       return "Type Error"
+    user = Member(username,password,1,name,email,phone)
     user_doc = {
         "username": user.username,
         "password": user.password,
